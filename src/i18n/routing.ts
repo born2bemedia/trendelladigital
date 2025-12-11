0c501e5d844b153bd5c57b74535ee20fb57878b1
SergWebAcceptance <serg.web@acceptance.ltd>
2025-12-11 18:01:23 +0200

    Update default locale in routing configuration from 'ro' to 'en' for improved user experience.

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -3,5 +3,5 @@ import { defineRouting } from 'next-intl/routing';
 export const routing = defineRouting({
   locales: ['en', 'de', 'it', 'ro'],
   localeDetection: false,
-  defaultLocale: 'ro',
+  defaultLocale: 'en',
 });
